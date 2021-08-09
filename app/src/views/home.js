33ceaf242a62510e1dbaf204b14010f761e93647
unknown <jwalin12@berkeley.edu>
2021-08-09 12:21:22 +0530

    fix:dom-helpers npm

--- app/src/views/home.js
+++ app/src/views/home.js
@@ -1,21 +1,56 @@
-var react = require('react');
-var reactDOm = require('react-dom');
-const { defaultGroupByFn } = require('react-table');
+import React from "react";
+import { VStack, Input, Button, IconButton, Icon, Text, NativeBaseProvider, Center, Box } from "native-base";
+import { Ionicons, MaterialIcons } from '@expo/vector-icons';
+import { FontAwesome5 } from '@expo/vector-icons';
 
 
-const SearchBar = (
-<div className="p-8">
-  <div className="bg-white flex items-center rounded-full shadow-xl">
-    <input className="rounded-l-full w-full py-4 px-6 text-gray-700 leading-tight focus:outline-none" id="search" type="text" placeholder="Search">
-    
-    <div className="p-4">
-      <button className="bg-green-500 text-white rounded-full p-2 hover:bg-blue-400 focus:outline-none w-12 h-12 flex items-center justify-center">
-        icon
-      </button>
-      </div>
-    </input>
-  </div>
-</div>
-);
+function SearchBar(){
+  return (
+    <VStack space={8} width="100%">
+      <VStack width="100%" space={2}>
+        <Box>Cupertino</Box>
+        <Input
+          placeholder="Search"
+          variant="filled"
+          width="100%"
+          bg="gray.200"
+          borderRadius={10}
+          py={1}
+          px={2}
+          _web={{
+            _focus: { borderColor: 'muted.300', style: { boxShadow: 'none' } },
+            }}
+          InputLeftElement={<Icon size='sm' ml={2} size={5} color="gray.400" as={<Ionicons name="ios-search" />} />}
+        />
+      </VStack>
 
-export default SearchBar;
+      <VStack width="100%" space={2}>
+        <Box>Material</Box>
+        <Input
+          placeholder="Search People & Places"
+          bg="#fff"
+          width="100%"
+          borderRadius={4}
+          py={3}
+          px={1}
+          fontSize={14}
+           _web={{
+            _focus: { borderColor: 'muted.300', style: { boxShadow: 'none' } },
+            }}
+          InputLeftElement={<Icon size='sm' m={2} size={6} color="gray.400" as={<MaterialIcons name="search" />} />}
+          InputRightElement={<Icon size='sm' m={2} size={6} color="gray.400" as={<MaterialIcons name="mic" />} />}
+        />
+        </VStack>
+      </VStack>
+  )
+}
+
+export default function () {
+  return (
+    <NativeBaseProvider>
+      <Center flex={1} px={2}>
+        <SearchBar/>
+      </Center>
+    </NativeBaseProvider>
+  );
+}
