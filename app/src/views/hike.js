b6c1693c360bd3e8f32d3d779f51117a8c6f2c45
Jwalin Joshi <jwalinjoshi@MacBook-Pro.local>
2021-09-28 16:28:19 -0700

    fix: using state variables for status

--- app/src/views/hike.js
+++ app/src/views/hike.js
@@ -1,27 +1,31 @@
 
 import React, { useState } from 'react';
 
-import { DataTable, Provider, View, Button, Modal, Portal } from 'react-native-paper';
+import { DataTable, Provider, View, Button, Modal, Portal, TextInput } from 'react-native-paper';
 import { NativeBaseProvider, Heading, Box } from 'native-base';
 
 
 export default function HikeScreen({ route }) {
-    const [mode, setMode] = useState('Basic');
     const { hikeData } = route.params;
-    let hikeName = hikeData.name;
-    let trailStatus = hikeData.trailStatus;
-    let parkingLot = hikeData.parkingLot;
-    let bearSightings = hikeData.bearSightings;
+    const hikeName = hikeData.name;
+    const [mode, setMode] = useState('Basic');
+    const[trailStatus, setTrailStatus] = React.useState(hikeData.trailStatus);
+    const [parkingLotStatus, setParkingLotStatus] = React.useState(hikeData.parkingLot);
+    const [bearSightingsStatus, setBearSightingsStatus] =  React.useState(hikeData.bearSightings);
+    const [TrailStatusInputText, setTrailStatusInputText] = React.useState('');
+    const [ParkingLotInputText, setParkingLotInputText] = React.useState('');
+    const [BearSightingInputText, setBearSightingInputText] = React.useState('');
+
 
     return (
       <NativeBaseProvider>
            <Box textAlign="center" bg= 'white' flex= {1} safeAreaTop>
           <Heading my={6} textAlign="center" size="lg">{hikeName}</Heading>
-            <HikeTable trailStatus={trailStatus} parkingLot={parkingLot} bearSightings={bearSightings} />
+            <HikeTable/>
         </Box>
       </NativeBaseProvider>
     );
-}
+
 
 function HikeTable(props) {
   const [TrailStatusPopupVisible,setTrailStatusPopupVisible] = useState(false);
@@ -38,7 +42,7 @@ function HikeTable(props) {
       <Button color = 'green' dark = 'true' mode= "text" onPress = {() => setTrailStatusPopupVisible(true)}> Trail Status</Button>
     </DataTable.Cell>
     <DataTable.Cell>
-      {props.trailStatus}
+      {trailStatus}
     </DataTable.Cell>
 </DataTable.Row>
   <DataTable.Row>
@@ -46,7 +50,7 @@ function HikeTable(props) {
     <Button color = 'green' dark = 'true' mode= "text" onPress = {() => setParkingLotPopupVisible(true)}> Parking Lot </Button>
     </DataTable.Cell>
     <DataTable.Cell>
-      {props.parkingLot}
+      {parkingLotStatus}
     </DataTable.Cell>
 </DataTable.Row>
 <DataTable.Row>
@@ -54,22 +58,27 @@ function HikeTable(props) {
     <Button color = 'green' dark = 'true' mode= "text" onPress = {() => setBearStatusPopupVisible(true)}>  Bear Sightings </Button>
     </DataTable.Cell>
     <DataTable.Cell>
-      {props.bearSightings}
+      {bearSightingsStatus}
     </DataTable.Cell>
 </DataTable.Row> 
 <Provider>
   <Portal>
 <Modal visible = {TrailStatusPopupVisible} contentContainerStyle = {containerStyle}>
+  {trailStatus}
+  <TextInput label = "Updates?">
+    </TextInput>
   <Button icon="alpha-x-circle" mode="contained" color = "red" dark = "true" onPress = {() => setTrailStatusPopupVisible(false)}>Cancel</Button>
 </Modal>
   </Portal>
   <Portal>
 <Modal visible = {ParkingLotStatusPopupVisible} contentContainerStyle = {containerStyle}>
+{parkingLotStatus}
   <Button icon="alpha-x-circle" mode="contained" color = "red" dark = "true" onPress = {() => setParkingLotPopupVisible(false)}>Cancel</Button>
 </Modal>
   </Portal>
   <Portal>
 <Modal visible = {BearSightingStatusPopupVisible} contentContainerStyle = {containerStyle}>
+  {bearSightingsStatus}
   <Button icon="alpha-x-circle" mode="contained" color = "red" dark = "true" onPress = {() => setBearStatusPopupVisible(false)}>Cancel</Button>
 </Modal>
   </Portal>
@@ -77,6 +86,8 @@ function HikeTable(props) {
 </DataTable>
 
   );
+
+}
 
 }
 
